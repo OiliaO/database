91d6a665ab449b74d850cd49116c5a001c0132c5
OiliaO <s.iliarabee2006@gmail.com>
2025-04-06 02:13:04 +0330

    add base EntityNotFoundException

--- src/db/exception/EntityNotFoundException.java
+++ src/db/exception/EntityNotFoundException.java
@@ -1,4 +1,15 @@
 package db.exception;
 
-public class EntityNotFoundException {
+public class EntityNotFoundException extends Exception {
+    public EntityNotFoundException() {
+        super("Cannot find entity");
+    }
+
+    public EntityNotFoundException(String message) {
+        super("Cannot find entity: " + message);
+    }
+
+    public EntityNotFoundException(int id) {
+        super("Cannot find entity with id = " + id);
+    }
 }
