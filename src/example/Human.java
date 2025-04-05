69a2b7ada62efbe6f776bf3713cde9816cefc6e8
OiliaO <s.iliarabee2006@gmail.com>
2025-04-06 03:10:18 +0330

    add base Human class

--- src/example/Human.java
+++ src/example/Human.java
@@ -1,4 +1,10 @@
 package example;
+import db.Entity;
 
-public class Human {
+public class Human extends Entity {
+    public String name;
+
+    public Human(String name) {
+        this.name = name;
+    }
 }
