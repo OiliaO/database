619bd0669fbf6ea7860b06e2f3cd9a5e4a3bc7a2
OiliaO <s.iliarabee2006@gmail.com>
2025-04-06 02:00:51 +0330

    add base database class

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,4 +1,24 @@
 package db;
+import java.util.ArrayList;
 
 public class Database {
+    private static Database instance;
+    private ArrayList<Entity> entities;
+    private int newId = 1;
+
+    private Database() {
+        entities = new ArrayList<>();
+    }
+
+    public static Database getInstance() {
+        if (instance == null) {
+            instance = new Database();
+        }
+        return instance;
+    }
+
+    public void add(Entity entity) {
+        entity.id = newId++;
+        entities.add(entity);
+    }
 }
