c2ae4eaca1df11630da8cd25725fa858ac3dc2d6
OiliaO <s.iliarabee2006@gmail.com>
2025-04-11 02:43:16 +0330

    update data base class

--- src/db/Database.java
+++ src/db/Database.java
@@ -1,7 +1,7 @@
 package db;
 
 import db.exception.*;
-import java.util.*;;
+import java.util.*;
 
 public class Database {
     private static ArrayList<Entity> entities = new ArrayList<>();
@@ -23,6 +23,69 @@ public class Database {
 
         Entity copy = entity.copy();
         copy.id = nextId++;
+
+        if (copy instanceof Trackable) {
+            Trackable trackable = (Trackable) copy;
+            Date now = new Date();
+            trackable.setCreationDate(now);
+            trackable.setLastModificationDate(now);
+        }
+
         entities.add(copy);
+    }
+
+    public static Entity get(int id) throws EntityNotFoundException {
+        for (Entity entity : entities) {
+            if (entity.id == id) {
+                if (entity instanceof Trackable) {
+                    Trackable trackable = (Trackable) entity;
+                    return entity.copy();
+                }
+                return entity.copy();
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static void update(Entity entity) throws InvalidEntityException, EntityNotFoundException {
+        Validator validator = validators.get(entity.getEntityCode());
+        if (validator != null) {
+            validator.validate(entity);
+        }
+
+        int index = -1;
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == entity.id) {
+                index = i;
+                break;
+            }
+        }
+        if (index == -1) {
+            throw new EntityNotFoundException(entity.id);
+        }
+
+        Entity copy = entity.copy();
+        if (copy instanceof Trackable) {
+            ((Trackable) copy).setLastModificationDate(new Date());
+        }
+        entities.set(index, copy);
+    }
+
+    public static void delete(int id) throws EntityNotFoundException {
+        for (int i = 0; i < entities.size(); i++) {
+            if (entities.get(i).id == id) {
+                entities.remove(i);
+                return;
+            }
+        }
+        throw new EntityNotFoundException(id);
+    }
+
+    public static List<Entity> getAll() {
+        List<Entity> copies = new ArrayList<>();
+        for (Entity entity : entities) {
+            copies.add(entity.copy());
+        }
+        return copies;
     }
 }
