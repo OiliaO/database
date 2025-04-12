ee7125a2795dfd9b2201f48cfc3a07b34fb88697
OiliaO <s.iliarabee2006@gmail.com>
2025-04-12 17:30:30 +0330

    update taskservice class

--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -1,4 +1,23 @@
 package todo.service;
 
+import db.Database;
+import db.exception.*;
+import todo.entity.Task;
+
 public class TaskService {
+    public static void setAsCompleted(int taskId) throws
+            EntityNotFoundException, InvalidEntityException {
+
+        Task task = (Task) Database.get(taskId);
+        task.setStatus(Task.Status.COMPLETED);
+        Database.update(task);
+    }
+
+    public static void updateTaskTitle(int taskId, String newTitle) throws
+            EntityNotFoundException, InvalidEntityException {
+
+        Task task = (Task) Database.get(taskId);
+        task.setTitle(newTitle);
+        Database.update(task);
+    }
 }
