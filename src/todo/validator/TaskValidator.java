ed7eee94e299e4901529dee92678510053a2b85c
OiliaO <s.iliarabee2006@gmail.com>
2025-04-12 17:25:27 +0330

    update TaskValidator class

--- src/todo/validator/TaskValidator.java
+++ src/todo/validator/TaskValidator.java
@@ -1,4 +1,21 @@
 package todo.validator;
 
-public class TaskValidator {
+import db.*;
+import db.exception.*;
+import todo.entity.Task;
+
+public class TaskValidator implements Validator {
+    @Override
+    public void validate(Entity entity) throws InvalidEntityException {
+        if (!(entity instanceof Task)) {
+            throw new IllegalArgumentException("Entity must be Task");
+        }
+        Task task = (Task) entity;
+        if (task.getTitle() == null || task.getTitle().isEmpty()) {
+            throw new InvalidEntityException("Task title cannot be empty");
+        }
+        if (task.getDueDate() == null) {
+            throw new InvalidEntityException("Due date must be set");
+        }
+    }
 }
