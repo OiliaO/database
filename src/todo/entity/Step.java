1ecbd92f04de3baa5d7672e0860515ae0b55b98a
OiliaO <s.iliarabee2006@gmail.com>
2025-04-12 17:41:26 +0330

    update Step class

--- src/todo/entity/Step.java
+++ src/todo/entity/Step.java
@@ -1,4 +1,55 @@
 package todo.entity;
 
-public class Step {
+import db.Entity;
+import java.util.Date;
+
+public class Step extends Entity {
+    public enum Status { NOT_STARTED, COMPLETED }
+
+    private String title;
+    private Status status;
+    private int taskRef;
+
+    public Step(String title, int taskRef) {
+        this.title = title;
+        this.taskRef = taskRef;
+        this.status = Status.NOT_STARTED;
+    }
+
+    @Override
+    public Step copy() {
+        Step copy = new Step(this.title, this.taskRef);
+        copy.id = this.id;
+        copy.status = this.status;
+        return copy;
+    }
+
+    @Override
+    public int getEntityCode() {
+        return 21;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public int getTaskRef() {
+        return taskRef;
+    }
+
+    public void setTaskRef(int taskRef) {
+        this.taskRef = taskRef;
+    }
 }
--- src/todo/service/TaskService.java
+++ src/todo/service/TaskService.java
@@ -5,16 +5,14 @@ import db.exception.*;
 import todo.entity.Task;
 
 public class TaskService {
-    public static void setAsCompleted(int taskId) throws
-            EntityNotFoundException, InvalidEntityException {
+    public static void setAsCompleted(int taskId) throws EntityNotFoundException, InvalidEntityException {
 
         Task task = (Task) Database.get(taskId);
         task.setStatus(Task.Status.COMPLETED);
         Database.update(task);
     }
 
-    public static void updateTaskTitle(int taskId, String newTitle) throws
-            EntityNotFoundException, InvalidEntityException {
+    public static void updateTaskTitle(int taskId, String newTitle) throws EntityNotFoundException, InvalidEntityException {
 
         Task task = (Task) Database.get(taskId);
         task.setTitle(newTitle);
